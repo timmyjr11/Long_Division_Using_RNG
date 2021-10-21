c81e0a8b2eaa3fd14c69bf880c142f2dfd99061a
timmyjr11 <timmyytjr@gmail.com>
2021-10-20 21:58:31 -0700

    Final Product for POW

--- Long_Division_Final.py
+++ Long_Division_Final.py
@@ -1,5 +1,6 @@
 import random
 
+# Creates lists that represent the numbers 0 - 9
 divisorHundred = [100, 200, 300, 400, 500, 600, 700, 800, 900]
 divisorTens = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 divisorOnes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -16,37 +17,44 @@ divisionOne = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 AnswerTenThousand = [0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000]
 AnswerOne = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-dividind = random.choice(divisionTenMillion) + random.choice(divisionOneMillion) + \
+# Creates the dividend by adding all the lists together while also adding a random selector
+dividend = random.choice(divisionTenMillion) + random.choice(divisionOneMillion) + \
            random.choice(divisionHundredThousand) \
            + random.choice(divisionTenThousand) + random.choice(divisionOneThousand) + \
            random.choice(divisionHundred) + random.choice(divisionTen) + random.choice(divisionOne)
 
 divisor = random.choice(divisorHundred) + random.choice(divisorTens) + random.choice(divisorOnes)
-# Fix
+
 Answer = random.choice(AnswerTenThousand) + 0 + 800 + 0 + random.choice(AnswerOne)
 
+# Variable "true" created to be able to switch the while statement from true to false
 true = True
 
+# Main part of the program that is a loop at that allows the numbers to be switched and changed
 while true:
-
-    dividind = random.choice(divisionTenMillion) + random.choice(divisionOneMillion) + \
+    # Randomizes the numbers then adds them together
+    dividend = random.choice(divisionTenMillion) + random.choice(divisionOneMillion) + \
                random.choice(divisionHundredThousand) \
                + random.choice(divisionTenThousand) + random.choice(divisionOneThousand) + \
                random.choice(divisionHundred) + random.choice(divisionTen) + random.choice(divisionOne)
 
     divisor = random.choice(divisorHundred) + random.choice(divisorTens) + random.choice(divisorOnes)
-    # Fix
+
     Answer = random.choice(AnswerTenThousand) + 0 + 800 + 0 + random.choice(AnswerOne)
 
-    print("dividind:" + str(dividind))
+    # Gives information of the different numbers used
+    print("dividend:" + str(dividend))
     print("divisor: " + str(divisor))
     print("Answer: " + str(Answer))
-
-    if Answer == (dividind / divisor) and (dividind % divisor) == 0:
+    # An if statement that states that if the answer is equal to the dividend divided by the divisor and the
+    # remainder is zero
+    if Answer == (dividend / divisor) and (dividend % divisor) == 0:
+        # If the statement is true, then stop randomizing the numbers and tell the correct answers
         true = False
-        print("Final dividend: " + str(dividind))
+        print("Final dividend: " + str(dividend))
         print("Final divisor: " + str(divisor))
         print("Final Answer:" + str(Answer))
-
+    # If the if statement is not true, then the code will state that the set of numbers failed to meet the conditions
+    # and move on to the next number
     else:
         print("fail")
