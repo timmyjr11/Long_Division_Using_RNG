fdfa1401c723e7a61aebe8f6cbc1bf36a9f8d688
timmyjr11 <timmyytjr@gmail.com>
2021-10-20 21:29:40 -0700

    Final Product for POW

--- Long_Division_Final.py
+++ Long_Division_Final.py
@@ -4,8 +4,6 @@ divisorHundred = [100, 200, 300, 400, 500, 600, 700, 800, 900]
 divisorTens = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 divisorOnes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-
-
 divisionTenMillion = [0, 10000000, 20000000, 30000000, 40000000, 50000000, 60000000, 70000000, 80000000, 90000000]
 divisionOneMillion = [0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000]
 divisionHundredThousand = [0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000]
@@ -28,7 +26,6 @@ divisor = random.choice(divisorHundred) + random.choice(divisorTens) + random.ch
 Answer = random.choice(AnswerTenThousand) + 0 + 800 + 0 + random.choice(AnswerOne)
 
 true = True
-
 
 while true:
 
